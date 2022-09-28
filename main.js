207160f0336e4bc19936ace8f16d212ea04677a2
Tomek Sosniewski <sosna86@gmail.com>
2022-09-28 15:40:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getAllMsg()
+})
+let btn = document.querySelector('#btn');
+let notify = document.querySelector('#notify')
+let allMsg = null;
+let board = document.querySelector('#board')
+let textArea = document.querySelector('#textArea');
+let submit = document.querySelector('#submit')
+
+function getAllMsg () {
+    allMsg = document.querySelectorAll('.unread').length
+    notify.textContent = allMsg;
+    return allMsg;
+}
+
+function clearNotifications () {    
+    let msg = document.querySelectorAll('.msg')
+    msg.forEach(e => {
+       e.classList.remove('unread')
+    })
+    notify.textContent = getAllMsg()
+}
+
+function newMsg () {    
+    if(textArea.value == ""){
+        return
+    }else{
+    let resp = document.createElement('div');
+    resp.classList.add('msg')
+    resp.classList.add('unread')
+    resp.textContent = textArea.value
+    board.append(resp)
+    allMsg += 1; 
+    notify.textContent = allMsg;  
+    textArea.value = " "
+}
+}
+
+
+submit.addEventListener('click', newMsg)
+btn.addEventListener('click', clearNotifications)
+
+
