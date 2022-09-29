e16726ded625cb8e5b4a2a51b5ae730383289f4d
Tomek Sosniewski <sosna86@gmail.com>
2022-09-29 15:02:14 +0200

    buttons function w/o functionality

--- main.js
+++ main.js
@@ -5,68 +5,113 @@ let btn = document.querySelector('#btn');
 let notify = document.querySelector('#notify')
 let allMsg = null;
 let board = document.querySelector('#board')
-let textArea = document.querySelector('#textArea');
-let submit = document.querySelector('#submit')
+let textArea = document.querySelector('#textAreaMain');
+let submit = document.querySelector('#submitMain')
 let upVote = document.getElementsByClassName('voteUp')
 
-function getAllMsg () {
+function getAllMsg() {
     allMsg = document.querySelectorAll('.unread').length
     notify.textContent = allMsg;
     return allMsg;
 }
 
-function clearNotifications () {    
+function clearNotifications() {
     let msg = document.querySelectorAll('.msg')
     msg.forEach(e => {
-       e.classList.remove('unread')
+        e.classList.remove('unread')
     })
     notify.textContent = getAllMsg()
 }
 
+function createVotingButtons(appendPlace, elementType, elementClassNr1, elementClassNr2, value) {
+    let element = document.createElement(elementType);
+    element.classList.add(elementClassNr1, elementClassNr2)
+    element.textContent = value
+    appendPlace.append(element)
+}
 
-function newMsg () {    
-    if(textArea.value == ""){
+function createButtonOrTextArea (elementType, className, value, appendPlace ){
+    let element = document.createElement(elementType)
+    element.classList.add(className)
+    element.textContent = value
+    appendPlace.append(element)
+}
+
+
+
+function newMsg() {
+
+    if (textArea.value == "") {
         return
-    }else{
-    let resp = document.createElement('div');
-    resp.classList.add('msg')
-    resp.classList.add('unread')
-    resp.innerHTML = `<span class="name">Tomek Sośniewski</ span> <span style="font-weight: 100">${textArea.value}</span><a class='msgLink' href='#'>... read message</a></br><span class='timeAgo' style="font-weight: 100">1min ago</span>`
-    board.append(resp)
-    allMsg += 1; 
-    notify.textContent = allMsg;  
-    textArea.value = " "
-}
-}
+    } else {
+        let resp = document.createElement('div');
+        let buttonContainer = document.createElement('div')
+        buttonContainer.classList.add('voting');
+
+        board.append(resp)
+        resp.append(buttonContainer)
+
+        resp.classList.add('msg', 'unread')
+        resp.innerHTML = `<span class="name">Tomek Sośniewski</ span> <span style="font-weight: 100">${textArea.value}</span><a class='msgLink' href='#'>... read message</a></br><span class='timeAgo' style="font-weight: 100">1min ago</span><div class='options'>      
+        <div class="response responseVisible">
+        <textarea class="textArea"></textarea><input type='submit' class='submit' value='submit'></input>
+        </div>
+        </div>`
+        // createButtonOrTextArea('textArea', 'textArea', null, resp)
+        createButtonOrTextArea('div', 'replay', 'replay', resp)
+        allMsg += 1;
+        notify.textContent = allMsg;
+        textArea.value = " "
 
 
-function voting () {
+
+        showReplay()
+
+        // displayVoting()
+
+    }
+}
+// function displayVoting(){
+//     let boxForVoting = document.querySelector('#board')
+//     let voting = document.createElement('div').classList.add('voting')
+//     let voteUp = document.createElement('button').classList.add('voteBtn', 'voteUp')
+//     let voteScore = document.createElement('div')
+//     voteScore.classList.add('voteBtn', 'voteNum')
+//     voteScore.textContent="0"
+//     let voteDown = document.createElement('button').classList.add('voteBtn', 'voteDown')
+//     console.log(boxForVoting)
+//     boxForVoting.append(voteScore)
+
+// }
+
+function voting() {
     let upVote = document.getElementsByClassName('voteUp')
     let downVote = document.getElementsByClassName('voteDown')
     for (let i = 0; i < upVote.length; i++) {
-      upVote[i].onclick = function() {
-        let div = this.nextElementSibling; 
-        div.textContent ++}
-      downVote[i].onclick = function() {
-        let div = this.previousElementSibling;
-        div.textContent --}  
-       } 
+        upVote[i].onclick = function () {
+            let div = this.nextElementSibling;
+            div.textContent++
+        }
+        downVote[i].onclick = function () {
+            let div = this.previousElementSibling;
+            div.textContent--
+        }
     }
+}
 
 voting()
 
-function showReplay () {
+function showReplay() {
     let replayBtn = document.querySelectorAll('.replay');
     let response = document.querySelectorAll('.response')
     for (let i = 0; i < replayBtn.length; i++) {
-        replayBtn[i].onclick = function() {
-        response[i].classList.toggle('responseVisible')
-        }  
-         } 
+        replayBtn[i].onclick = function () {
+            response[i].classList.toggle('responseVisible')
+        }
+    }
 }
 
 showReplay()
 
 submit.addEventListener('click', newMsg)
 btn.addEventListener('click', clearNotifications)
-
